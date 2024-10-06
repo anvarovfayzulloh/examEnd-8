046858607d199963e3f49fdc15b06bbc3ab9b9f8
anvarovfayzulloh <fayzee200708@gmail.com>
2024-10-06 18:58:47 +0300

    nav top

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,3 @@
-// import { AiFillStar } from "react-icons/ai"; 
 import { Link } from "react-router-dom";
 import { Container } from "../../utils";
 import { Tooltip } from 'antd';
@@ -7,32 +6,33 @@ const Nav = () => {
     return (
         <header>
             <Container>
-                <div>
+                <div className="nav-bar-top" >
                     <ul className="flex justify-between items-center h-10 border-b border-[#ebebeb] text-[#212121]" >
-                        <li className="hover:text-[#5b24c9] cursor-pointer font-pixel" >
+                        <li className="hover:text-[#5b24c9] cursor-pointer font-fixel text-[14px] flex basis-1/4 flex-grow-0 items-center justify-start">
                             <Tooltip overlayStyle={{ borderRadius: '1px' , maxWidth: '264px'}} placement="bottomRight" title="Мы доставляем парфюмерию и косметику бесплатно. Минимальная сумма заказов - 99 000 сумов">
                                 <span>Бесплатная доставка!</span>
                             </Tooltip>
-                            {/* 100% Оригинальная продукция! #TODO two version in this text */}
                         </li>
-                        <ul className="flex justify-between items-center gap-5 cursor-pointer" >
-                            <li className="text-[#ff5252]" >
+                        <ul className="flex justify-center items-center gap-5 cursor-pointer text-[14px]  flex-1" >
+                            <li className="text-[#ff5252] font-fixel" >
                                 <Link to={"/"}>Акции</Link>
                             </li>
-                            <li className="hover:text-[#5b24c9] cursor-pointer" >
+                            <li className="hover:text-[#5b24c9] cursor-pointer font-fixel text-[14px]" >
                                 <Link to={"/"} >Доставка и Оплата</Link>
                             </li>
-                            <li className="hover:text-[#5b24c9] cursor-pointer" >
+                            <li className="hover:text-[#5b24c9] cursor-pointer font-fixel text-[14px]" >
                                 <Link to={"/"} >О магазине</Link>
                             </li>
                         </ul>
-                        <li className="flex items-center gap-2 hover:text-[#5b24c9] cursor-pointer" >
-                            {/* <AiFillStar className="w-[10px] h-[10px]"  color="black" /> */}
-                            <span className="">★</span>
+                        <li className="gap-2 hover:text-[#5b24c9] cursor-pointer font-fixel text-[14px] flex basis-1/4 flex-grow-0 items-center justify-end" >
+                            <span>★</span>
                             Beauty Club
                         </li>
                     </ul>
                 </div>
+                <nav className="nav-bar-bottom" >
+
+                </nav>
             </Container>
         </header>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        pixel: ['PixelText'],
+        fixel: ['FixelText'],
       },
     },
   },
