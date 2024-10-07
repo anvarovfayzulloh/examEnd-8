c22768402a8ad568061a08e315e569e3c77091c0
anvarovfayzulloh <fayzee200708@gmail.com>
2024-10-07 15:45:38 +0300

    carousel finish

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -7,22 +7,23 @@ import Carousel_5 from "../../assets/images/carousel_5.jpg";
 import Arrow from "../../assets/images/arrow.svg";
 import { Container } from '../../utils';
 import { ArrowProps } from '../../types';
+import './CarouselHeader.css';
 
 const CustomArrowLeft: React.FC<ArrowProps> = ({ className, style, onClick }) => {
     return (
         <div
-            className={className}
+            className={`carousel-arrow left ${className}`}
             style={{
                 padding: "10px",
                 ...style,
                 background: "white",
                 backgroundRepeat: 'no-repeat',
-                width: '60px',
-                height: '60px',
+                width: '50px',
+                height: '50px',
                 position: 'absolute',
-                top: '270px',
+                top: '50%',
                 left: '10px',
-                transform: 'rotate(180deg)',
+                transform: 'translateY(-50%) rotate(180deg)',
                 cursor: 'pointer',
                 zIndex: 10,
             }}
@@ -33,18 +34,19 @@ const CustomArrowLeft: React.FC<ArrowProps> = ({ className, style, onClick }) =>
 const CustomArrowRight: React.FC<ArrowProps> = ({ className, style, onClick }) => {
     return (
         <div
-            className={className}
+            className={`carousel-arrow right ${className}`}
             style={{
                 ...style,
                 padding: "10px",
                 background: "white",
                 backgroundSize: 'contain',
                 backgroundRepeat: 'no-repeat',
-                width: '60px',
-                height: '60px',
+                width: '50px',
+                height: '50px',
                 position: 'absolute',
                 top: '50%',
                 right: '10px',
+                transform: 'translateY(-50%)',
                 cursor: 'pointer',
                 zIndex: 10,
             }}
@@ -55,24 +57,23 @@ const CustomArrowRight: React.FC<ArrowProps> = ({ className, style, onClick }) =
 const CarouselHeader: React.FC = () => {
     return (
         <Container>
-            <div style={{ position: 'relative' }}>
-
+            <div className="carousel-wrapper">
                 <ConfigProvider
                     theme={{
                         components: {
-                          Carousel: {
-                            dotWidth: 5,
-                            dotHeight: 5,
-                            dotActiveWidth: 10,
-                          }, 
+                            Carousel: {
+                                dotWidth: 5,
+                                dotHeight: 5,
+                                dotActiveWidth: 10,
+                            },
                         },
-                      }}
+                    }}
                 >
-                    <Carousel effect="fade" arrows={true} nextArrow={<CustomArrowRight />} prevArrow={<CustomArrowLeft />} >
+                    <Carousel autoplay autoplaySpeed={4000} effect="fade" arrows={true} nextArrow={<CustomArrowRight />} prevArrow={<CustomArrowLeft />} >
                         <div>
                             <img src={Carousel_1} alt="" />
                         </div>
-                        <div ons >
+                        <div>
                             <img src={Carousel_2} alt="" />
                         </div>
                         <div>
