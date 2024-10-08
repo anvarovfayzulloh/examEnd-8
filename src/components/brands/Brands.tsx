eb7126218b2cd714119866096116e9da14fa4a9f
anvarovfayzulloh <fayzee200708@gmail.com>
2024-10-08 12:57:26 +0300

    finish

--- src/components/brands/Brands.tsx
+++ src/components/brands/Brands.tsx
@@ -1,9 +1,19 @@
 import { useEffect } from "react";
 import { useGetProductQuery } from "../../redux/api/productsApi"
 import { Link } from "react-router-dom";
+import Categories_1 from "../../assets/images/categories_1.avif"
+import Categories_2 from "../../assets/images/categories_2.jpg"
+import Categories_3 from "../../assets/images/categories_3.webp"
+import Categories_4 from "../../assets/images/categories_4.jpg"
+import Categories_5 from "../../assets/images/categories_5.avif"
+import Categories_6 from "../../assets/images/categories_6.avif"
+import Categories_7 from "../../assets/images/categories_7.avif"
+import Categories_8 from "../../assets/images/categories_8.webp"
+import Categories_9 from "../../assets/images/categories_9.jpg"
+import Categories_10 from "../../assets/images/categories_10.jpg"
 
 
-const Brands = () => {
+const Categories = () => {
     const { data } = useGetProductQuery(undefined);
     useEffect(() => {
         console.log(data)
@@ -13,58 +23,102 @@ const Brands = () => {
             <ul className="capitalize flex justify-center items-center gap-4" >
                 <li>
                     <Link to={"/"} >
-                    <img src="" alt="" />
-                        nyx
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_1} alt="" />
+                        </div>
+                        <p>
+                            Blush
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        clinique
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_2} alt="" />
+                        </div>
+                        <p>
+                            Bronzer
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        dior
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_3} alt="" />
+                        </div>
+                        <p>
+                            Eyebrow
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        smashbox
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_4} alt="" />
+                        </div>
+                        <p>
+                            Eyeliner
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        e.l.f.
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_5} alt="" />
+                        </div>
+                        <p>
+                            Eyeshadow
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        milani
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_6} alt="" />
+                        </div>
+                        <p>
+                            Foundation
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        rejuva minerals
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_7} alt="" />
+                        </div>
+                        <p>
+                            Lip liner
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        annabelle
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_8} alt="" />
+                        </div>
+                        <p>
+                            Lipstick
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        pure anada
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_9} alt="" />
+                        </div>
+                        <p>
+                            Mascara
+                        </p>
                     </Link>
                 </li>
                 <li>
                     <Link to={"/"} >
-                        pacifica
-                    </Link>
-                </li>
-                <li>
-                    <Link to={"/"} >
-                        almay
+                        <div className="w-[82px] h-[82px] p-4 border border-[#5b24c9] rounded-full" >
+                            <img className="w-full h-full" src={Categories_10} alt="" />
+                        </div>
+                        <p>
+                            Nail polish
+                        </p>
                     </Link>
                 </li>
             </ul>
@@ -72,4 +126,4 @@ const Brands = () => {
     )
 }
 
-export default Brands
+export default Categories
--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from '../../components/nav/Nav';
 import Menu from '../../components/menu/Menu';
 import CarouselHeader from '../../components/slider/Slider';
-import Brands from '../../components/brands/Brands';
+import Categories from '../../components/brands/Brands';
 
 const Home: React.FC = () => {
 
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
             <Nav/>
             <Menu/>
             <CarouselHeader/>
-            <Brands/>
+            <Categories/>
         </>     
     );
 };
