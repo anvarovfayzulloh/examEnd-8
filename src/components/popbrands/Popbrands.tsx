5c641e650aca41f4c803318ebfb9073e1aa3d077
anvarovfayzulloh <fayzee200708@gmail.com>
2024-10-26 15:16:39 +0300

    finish

--- src/components/popbrands/Popbrands.tsx
+++ src/components/popbrands/Popbrands.tsx
@@ -12,7 +12,6 @@ const Popbrands = () => {
   const [productsv2, setProductsv2] = useState([]);
   const [productsv3, setProductsv3] = useState([]);
   const [productsv4, setProductsv4] = useState([]);
-  const [isModalVisible, setIsModalVisible] = useState(true);
 
   useEffect(() => {
     setProducts(data?.slice(0, 20));
@@ -31,32 +30,10 @@ const Popbrands = () => {
       </div>
     );
   }
-  const handleCloseModal = () => {
-    setIsModalVisible(false);
-  };
 
   return (
     <div>
       <Container>
-      {isModalVisible && (
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50 ">
-            <div className="bg-white p-5 rounded shadow-lg text-center  max-w-[420px]">
-              <h2 className="text-xl font-semibold">Important Notice</h2>
-              <p className="mt-4 mb">
-              Please note that the project is done approximately 24 hours before the exam. Thank you for your understanding.
-              </p>
-              <p>
-
-              </p>
-              <button
-                onClick={handleCloseModal}
-                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        )}
         <h1 className="flex justify-center items-center h-3">
           <span className="mt-[120px] font-fixel text-[26px] font-medium align-center">
             Предложения Брендов
